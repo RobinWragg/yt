984405fbec2e1a8e26c73c02b9688abd0e5cccf2
Robin Wragg <robin.wragg@me.com>
2024-06-24 13:58:10 +0100

    Basic sorting using table headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from "react";
 import './App.css';
 import mockdatafile from "./mockdata.json";
 
@@ -18,23 +19,42 @@ const columns = [
 ];
 
 function Table() {
-    function compareEntries(a: TableEntry, b: TableEntry) {
-        if (a.title < b.title) {
-            return -1;
-        } else if (a.title > b.title) {
+    const [sortMode, setSortMode] = useState("");
+
+    function compareTitles(a: TableEntry, b: TableEntry) {
+        let compareResult = a.title.localeCompare(b.title);
+        if (compareResult > 0) {
             return 1;
+        } else if (compareResult < 0) {
+            return -1;
+        }
+        return 0;
+    }
+
+    function compareChannels(a: TableEntry, b: TableEntry) {
+        let compareResult = a.channel.localeCompare(b.channel);
+        if (compareResult > 0) {
+            return 1;
+        } else if (compareResult < 0) {
+            return -1;
         }
         return 0;
     }
 
     let mockData: TableEntry[] = mockdatafile;
-    mockData = mockData.sort(compareEntries);
+
+    console.log(sortMode);
+    if (sortMode == "title") {
+        mockData = mockData.sort(compareTitles);
+    } else if (sortMode == "channel") {
+        mockData = mockData.sort(compareChannels);
+    }
 
     return <table>
         <thead>
             <tr>
                 {columns.map(({ label, accessor }) => {
-                    return <th key={accessor}>{label}</th>;
+                    return <th onClick={() => setSortMode(accessor)} key={accessor}>{label}</th>;
                 })}
                 <th>Actions</th>
             </tr>
