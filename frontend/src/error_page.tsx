48f2adfd8bf28c6120800961b79ba8c539c44bf7
Robin Wragg <robin.wragg@me.com>
2024-07-10 10:15:49 +0100

    Fix error being 'unknown' type

--- frontend/src/error_page.tsx
+++ frontend/src/error_page.tsx
@@ -3,10 +3,25 @@ import { useRouteError } from "react-router-dom";
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
+  console.error(typeof error);
+
+  function errorMessage(error: unknown) {
+    if (error instanceof Error) {
+      return error.message;
+    } else if (
+      error instanceof Object &&
+      "statusText" in error &&
+      typeof error.statusText === "string"
+    ) {
+      return error.statusText;
+    } else {
+      return "Unknown Error";
+    }
+  }
 
   return (
     <div id="error-page">
-      <h1>{error.statusText || error.message}</h1>
+      <h1>{errorMessage(error)}</h1>
       <p>thismachinewillnotcommunicate</p>
     </div>
   );
